09a0037042de9e169dfd96dba22f58a4144ac578
santiagohewett <santiago.hewett@stu.ocsb.ca>
2023-09-20 09:43:37 +0000

    Finished Unit1-04-CPP

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Santiago Hewett All rights reserved.
+// .
+// Created by: Santiago Hewett
+// Created on: Sep. 19, 2023
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 5cm X 3cm.
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 3.8cm and width 7.5cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 3.8 * 7.5 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (3.8 + 7.5) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 Santiago Hewett All rights reserved.
+// .
+// Created by: Santiago Hewett
+// Created on: Sep. 19, 2023
+// This program does basic math.
+
+#include <cmath>
+#include <iostream>
+
+
+int main() {
+    std::cout << "9.2 + 2.6 = " << 9.2 + 2.6 << std::endl;
+    std::cout << "7.4 - 3.8 = " << 7.4 - 3.8 << std::endl;
+    std::cout << "10.4 / 2.9 = " << 10.4 / 2.9 << std::endl;
+    std::cout << "8.4 - 4.3 / 2 = " << 8.4 - 4.3 / 2 << std::endl;
+    std::cout << "2.2^3 = " << pow(2.2, 3) << std::endl;
+    std::cout << "1.7 / 3 = " << float(1.7) / float(3) << std::endl;
+}
